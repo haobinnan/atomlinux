3a483b1bcfa8266f1f5630c7c9c649ee69d84075
haobinnan <binnan_hao@qq.com>
2018-06-25 15:24:11 +0800

    Added secure boot function in ovmf firmware.

--- ovmf/mk-ovmf.sh
+++ ovmf/mk-ovmf.sh
@@ -8,6 +8,10 @@ fi
 #Load from VariableSetting file
 AtomLinux_OvmfVNumber="$(grep -i ^AtomLinux_OvmfVNumber ../VariableSetting | cut -f2 -d'=')"
 AtomLinux_DownloadURL="$(grep -i ^AtomLinux_OvmfURL ../VariableSetting | cut -f2 -d'=')"
+
+AtomLinux_SecureBootSignature="$(grep -i ^AtomLinux_SecureBootSignature ../VariableSetting | cut -f2 -d'=')"
+AtomLinux_OvmfOpenSSLVNumber="$(grep -i ^AtomLinux_OvmfOpenSSLVNumber ../VariableSetting | cut -f2 -d'=')"
+AtomLinux_OvmfOpenSSLURL="$(grep -i ^AtomLinux_OvmfOpenSSLURL ../VariableSetting | cut -f2 -d'=')"
 #Load from VariableSetting file
 
 OBJ_PROJECT=ovmf
@@ -41,21 +45,67 @@ mkdir ${OBJ_PROJECT}-tmp
 tar xzvf ./${FILENAME} -C ./${OBJ_PROJECT}-tmp
 #Check Decompression
 if [ ! $? -eq 0 ]; then
-    echo "Error: Decompression shim ."
+    echo "Error: Decompression ovmf ."
     exit 1
 fi
 #Check Decompression
 
+#OpenSSL
+if [ ${AtomLinux_SecureBootSignature} = "Yes" ]; then
+    FILENAME=${AtomLinux_OvmfOpenSSLVNumber}.tar.gz
+
+    #Download Source Code
+    if [ ! -f ./${FILENAME} ]; then
+        #Check if necessary tools are installed
+        if [ -z $(which wget) ]; then
+            echo "wget is not installed."
+            exit 1
+        fi
+        #Check if necessary tools are installed
+        wget ${AtomLinux_OvmfOpenSSLURL}${FILENAME}
+        if [ ! $? -eq 0 ]; then
+            echo "Error: Download ovmf_openssl ."
+            exit 1
+        fi
+        #Check if it is downloaded successfully
+        if [ ! -f ./${FILENAME} ]; then
+            echo "Error: Download ovmf_openssl ."
+            exit 1
+        fi
+    fi
+    #Download Source Code
+
+    tar xzvf ./${FILENAME} -C ./${OBJ_PROJECT}-tmp
+    #Check Decompression
+    if [ ! $? -eq 0 ]; then
+        echo "Error: Decompression ovmf_openssl ."
+        exit 1
+    fi
+    #Check Decompression
+
+    rm -rf ./${OBJ_PROJECT}-tmp/*-${AtomLinux_OvmfVNumber}/CryptoPkg/Library/OpensslLib/openssl
+    mv ./${OBJ_PROJECT}-tmp/openssl-*/ ./${OBJ_PROJECT}-tmp/edk2-${AtomLinux_OvmfVNumber}/CryptoPkg/Library/OpensslLib/openssl/
+fi
+#OpenSSL
+
 cd ./${OBJ_PROJECT}-tmp/*-${AtomLinux_OvmfVNumber}
 
 #IA32
-OvmfPkg/build.sh -a IA32
-cp -v Build/OvmfIa32/DEBUG_GCC*/FV/OVMF.fd ../../OVMFIA32.fd
+if [ ${AtomLinux_SecureBootSignature} = "Yes" ]; then
+    OvmfPkg/build.sh -a IA32 -b RELEASE -DSECURE_BOOT_ENABLE=TRUE
+else
+    OvmfPkg/build.sh -a IA32 -b RELEASE
+fi
+cp -v Build/OvmfIa32/RELEASE_GCC*/FV/OVMF.fd ../../OVMFIA32.fd
 #IA32
 
 #X64
-OvmfPkg/build.sh -a X64
-cp -v Build/OvmfX64/DEBUG_GCC*/FV/OVMF.fd ../../OVMFX64.fd
+if [ ${AtomLinux_SecureBootSignature} = "Yes" ]; then
+    OvmfPkg/build.sh -a X64 -b RELEASE -DSECURE_BOOT_ENABLE=TRUE
+else
+    OvmfPkg/build.sh -a X64 -b RELEASE
+fi
+cp -v Build/OvmfX64/RELEASE_GCC*/FV/OVMF.fd ../../OVMFX64.fd
 #X64
 
 #clean
