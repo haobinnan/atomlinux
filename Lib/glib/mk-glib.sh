36f15142da781cd0a372eb7420e8f67ff7f5f7f9
haobinnan <haobinnan@gmail.com>
2019-09-20 10:51:55 +0800

    glib updates to 2.61.3

--- Lib/glib/mk-glib.sh
+++ Lib/glib/mk-glib.sh
@@ -13,6 +13,8 @@ AtomLinux_DownloadURL="$(grep -i ^AtomLinux_GlibURL ../../VariableSetting | cut 
 AtomLinux_Only64Bit="$(grep -i ^AtomLinux_Only64Bit ../../VariableSetting | cut -f2 -d'=')"
 #Load from VariableSetting file
 
+UsingMeson=Yes
+
 OBJ_PROJECT=glib
 FILENAME_DIR=${OBJ_PROJECT}-$AtomLinux_GlibVNumber
 FILENAME=${FILENAME_DIR}.tar.xz
@@ -72,41 +74,82 @@ fi
 mkdir ./${ARCH}-${OBJ_PROJECT}
 cd ./${OBJ_PROJECT}-tmp/${FILENAME_DIR}
 
-#configure
+#configure | meson
 if [ ${AtomLinux_Only64Bit} = "Yes" ]; then
-    ./autogen.sh
-    ./configure --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} --enable-static --disable-shared CFLAGS="-static"
-else
-    if [ $(getconf LONG_BIT) = '64' ]; then
-        ./autogen.sh
-        ./configure --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} --enable-static --disable-shared CFLAGS="-static" CC="gcc -m32" CXX="g++ -m32"
+    if [ ${UsingMeson} = "Yes" ]; then
+        meson _build --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} -Ddefault_library=static
     else
         ./autogen.sh
         ./configure --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} --enable-static --disable-shared CFLAGS="-static"
     fi
+else
+    if [ ${UsingMeson} = "Yes" ]; then
+        if [ $(getconf LONG_BIT) = '64' ]; then
+            meson _build --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} -Ddefault_library=static --cross-file ../../meson_x86-linux
+        else
+            meson _build --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} -Ddefault_library=static
+        fi
+    else
+        if [ $(getconf LONG_BIT) = '64' ]; then
+            ./autogen.sh
+            ./configure --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} --enable-static --disable-shared CFLAGS="-static" CC="gcc -m32" CXX="g++ -m32"
+        else
+            ./autogen.sh
+            ./configure --prefix=$PWD/../../${ARCH}-${OBJ_PROJECT} --enable-static --disable-shared CFLAGS="-static"
+        fi
+    fi
 fi
-#configure
+#configure | meson
 
-#Check configure
-if [ ! $? -eq 0 ]; then
-    echo "Error: configure (glib) ."
-    exit 1
+if [ ${UsingMeson} = "Yes" ]; then
+    #Check meson
+    if [ ! $? -eq 0 ]; then
+        echo "Error: meson (glib) ."
+        exit 1
+    fi
+    #Check meson
+    ninja -C _build
+    #Check ninja
+    if [ ! $? -eq 0 ]; then
+        echo "Error: ninja (glib) ."
+        exit 1
+    fi
+    #Check ninja
+    ninja -C _build install
+    #Check ninja install
+    if [ ! $? -eq 0 ]; then
+        echo "Error: ninja install (glib) ."
+        exit 1
+    fi
+    #Check ninja install
+else
+    #Check configure
+    if [ ! $? -eq 0 ]; then
+        echo "Error: configure (glib) ."
+        exit 1
+    fi
+    #Check configure
+    echo | $Make
+    #Check make
+    if [ ! $? -eq 0 ]; then
+        echo "Error: make (glib) ."
+        exit 1
+    fi
+    #Check make
+    make install
+    #Check make install
+    if [ ! $? -eq 0 ]; then
+        echo "Error: make install (glib) ."
+        exit 1
+    fi
+    #Check make install
 fi
-#Check configure
-echo | $Make
-#Check make
-if [ ! $? -eq 0 ]; then
-    echo "Error: make (glib) ."
-    exit 1
+
+if [ ${UsingMeson} = "Yes" ]; then
+    mv ../../${ARCH}-${OBJ_PROJECT}/lib/${ARCH}-linux-gnu/* ../../${ARCH}-${OBJ_PROJECT}/lib/
+    rm -rf ../../${ARCH}-${OBJ_PROJECT}/lib/${ARCH}-linux-gnu
 fi
-#Check make
-make install
-#Check make install
-if [ ! $? -eq 0 ]; then
-    echo "Error: make install (glib) ."
-    exit 1
-fi
-#Check make install
+
 cd ../../
 rm -rf ${OBJ_PROJECT}-tmp
 
